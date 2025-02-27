49a2378dfeb0fecbbfccfd5e86f5eae8d3763103
mrGois <ezequielgois@yahoo.com.br>
2025-02-27 16:53:58 -0300

    adição da readline para digitação dos numeros

--- Calculadora C#/Program.cs
+++ Calculadora C#/Program.cs
@@ -7,6 +7,15 @@
             Console.WriteLine("Calculadora");
             Console.WriteLine("escolha a operação: +, -, *, /");
             Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro número:");
+            double num1 = convert.todouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+
         }
     }
 }
