0a8e646f4cfb2911607e99dd47d67d93006ae0d7
mrGois <ezequielgois@yahoo.com.br>
2025-02-27 17:27:48 -0300

    termino de codigo

--- Calculadora C#/Program.cs
+++ Calculadora C#/Program.cs
@@ -6,16 +6,50 @@
         {
             Console.WriteLine("Calculadora");
             Console.WriteLine("escolha a operação: +, -, *, /");
-            Console.ReadLine();
+            String? operacao = Console.ReadLine();
 
-            Console.WriteLine("Digite o primeiro número:");
-            double num1 = convert.todouble(Console.ReadLine());
+            if (string.IsNullOrEmpty(operacao))
+
+                Console.WriteLine("Digite o primeiro número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Digite o segundo número:");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
             double resultado = 0;
 
+            switch (operacao)
+            {
+                case "*":
+                    resultado = Multiplicar(num1, num2);
+                    break;
+                case "/":
+                    resultado = Dividir(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida.");
+                    break;
 
+            }
+            Console.WriteLine($"Resultado: {resultado}");
+
+            static double Multiplicar(double a, double b)
+            {
+                return a * b;
+            }
+
+            static double Dividir(double a, double b)
+            {
+                if (b != 0)
+                {
+                    return a / b;
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero.");
+                    return 0;
+
+                }
+            }
         }
     }
 }
