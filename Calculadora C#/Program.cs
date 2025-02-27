51b8fd0529e42ac37ca391a58fb4c66100aee349
AlineConchetta <alineconchetta@gmail.com>
2025-02-27 16:41:29 -0300

    Construção do código de adiçao e de subtração

--- Calculadora C#/Program.cs
+++ Calculadora C#/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double num1, num2, resultado;
+            string operacao;
+
+            Console.WriteLine("Escolha a operação que deseja realizar: -, +");
+            operacao = Console.ReadLine();
+            
+            Console.WriteLine("Digite o primeiro número:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (operacao)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    Console.WriteLine($"O resultado da soma {num1} e {num2} é de: {resultado}");
+                    break;
+
+                case "-":
+                    resultado = (num1 - num2);
+                    Console.WriteLine($"O resultado da subtração {num1} e {num2} é de: {resultado}");
+                    break;
+
+
+
+            }
         }
     }
 }
